dedb2cf398f6f7576dc9d7b51db30a844731e44d
Imam Syahrohim <syahrohim55@gmail.com>
2024-10-28 22:10:14 +0700

    Update profile.php

--- profile.php
+++ profile.php
@@ -8,6 +8,7 @@ if (!isset($_SESSION['username']) || !isset($_SESSION['email'])) {
     exit;
 }
 
+// Ambil data pengguna dari session
 $username = $_SESSION['username'];
 $email = $_SESSION['email'];
 ?>
@@ -18,7 +19,6 @@ $email = $_SESSION['email'];
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>User Profile</title>
-    <script type="module" src="scripts/index.js"></script>
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
