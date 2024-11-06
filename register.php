64850c98bbdda11024d12d5a5d83d4a5aaf1f79d
Natalio2512 <natavalen94@gmail.com>
2024-11-06 16:46:45 +0700

    profile

--- register.php
+++ register.php
@@ -48,6 +48,8 @@ if (isset($_POST['register'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Register Form</title>
+
+    <link rel="stylesheet" href="login.css">
 </head>
 <body>
     <div class="wrapper">
