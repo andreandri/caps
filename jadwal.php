466ed9f212979acd1b4fe0e7420dbdaf049b08f3
andreandri <165983950+andreandri@users.noreply.github.com>
2024-11-11 14:21:01 +0700

    perbaikan pada admin

--- jadwal.php
+++ jadwal.php
@@ -32,18 +32,19 @@
             <?php
             // Query untuk mengambil data jadwal
             $sql = "
-                SELECT 
-                    r.kota_asal, 
-                    r.kota_tujuan, 
-                    j.waktu_keberangkatan, 
-                    j.harga_tiket
-                FROM 
-                    jadwal j
-                JOIN 
-                    rute r ON j.id_rute = r.id_rute
-                ORDER BY 
-                    j.waktu_keberangkatan
-            ";
+                  SELECT 
+                      r.kota_asal, 
+                      r.kota_tujuan, 
+                      j.tgl_keberangkatan,
+                      j.jam_keberangkatan,
+                      j.harga
+                  FROM 
+                      tb_jadwal j
+                  JOIN 
+                      tb_rute r ON j.id_rute = r.id_rute
+                  ORDER BY 
+                      j.tgl_keberangkatan, j.jam_keberangkatan
+              ";
 
             $result = $koneksi->query($sql);
 
@@ -53,9 +54,9 @@
                     echo '<div class="schedule-item">';
                     echo '<div>';
                     echo '<h3>' . $row['kota_asal'] . ' - ' . $row['kota_tujuan'] . '</h3>';
-                    echo '<p>Jam Keberangkatan : ' . date("H.i", strtotime($row['waktu_keberangkatan'])) . ' WIB</p>';
-                    echo '</div>';
-                    echo '<div class="price">IDR ' . number_format($row['harga_tiket'], 0, ',', '.') . '</div>';
+                    echo '<p>Tanggal Keberangkatan : ' . date("d-m-Y", strtotime($row['tgl_keberangkatan'])) . '</p>';
+                    echo '<p>Jam Keberangkatan : ' . date("H.i", strtotime($row['jam_keberangkatan'])) . ' WIB</p>';
+                    echo '<div class="price">IDR ' . number_format($row['harga'], 0, ',', '.') . '</div>';
                     echo '</div>';
                 }
             } else {
